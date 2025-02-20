f3b6df4d8a7f8eb77b5877088de05b5bada8ec58
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2025-02-20 10:16:15 +0100

    Update HalakDTO.cs

--- DTOs/HalakDTO.cs
+++ DTOs/HalakDTO.cs
@@ -1,12 +1,13 @@
-﻿namespace halak.DTOs
+namespace halak.DTOs
 {
     public class HalakDTO
     {
         public int Id { get; set; }
         public string Nev { get; set; }
         public string Faj { get; set; }
-        public double MeretCm { get; set; }
+        public decimal MeretCm { get; set; }
         public string ToNev { get; set; }  // Tó neve
+        public string Kep { get; set; }
     }
 
 }
