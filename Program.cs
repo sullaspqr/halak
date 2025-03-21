57d1c2a956c332bf11690a0b077f1a8367f9a8ef
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2025-03-21 09:06:30 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,9 +16,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 var app = builder.Build();
-
+app.UseCors("AllowAllOrigins");
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 
